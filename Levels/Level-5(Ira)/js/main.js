1c05cbf0131ea915a728296f6b74eeb8db7ada89
nicoandrade50 <83848730+nicoandrade50@users.noreply.github.com>
2023-07-31 15:08:39 -0500

    Update main.js

--- Levels/Level-5(Ira)/js/main.js
+++ Levels/Level-5(Ira)/js/main.js
@@ -108,3 +108,87 @@ function moveship(){
 		}
 	}
 }
+function drawshotsenemies(){
+	for (const i in shotsenemies){
+		let Shooting = shotsenemies[i];
+		ctx.save();
+		ctx.fillStyle = 'yellow';
+		ctx.fillRect(Shooting.x, Shooting.y, Shooting.width, Shooting.height);
+		ctx.restore();
+	}
+}
+function moveshotsenemies(){
+	for(const i in shotsenemies){
+		let Shooting = shotsenemies[i];
+		Shooting.y += 2;
+	}
+	shotsenemies = shotsenemies.filter(function(Shooting){
+		return Shooting.y < canvas.height;
+	});
+}
+
+function updateenemies(){
+	function addshotsenemies(){
+		return{
+			x: enemy.x,
+			y: enemy.y,
+			width:10,
+			height:33,
+			counter: 0
+		}
+	}
+	
+	if(juego.state == 'iniciando'){
+		for ( let i= 0; i<10; i++){
+			enemies.push({
+				x: 10 + (i*50),
+				y: 10,
+				height: 40,
+				width: 40,
+				state: 'vivo',
+				counter: 0
+			});
+		}
+		juego.state = 'jugando';
+	}
+	for(const i in enemies){
+		var enemy = enemies[i];
+		if(!enemy) continue;
+		if (enemy && enemy.state == 'vivo'){
+			enemy.counter++;
+			enemy.x += Math.sin(enemy.counter * Math.PI / 90)*5;
+
+			if(aleatorio(0,enemies.length * 10 ) == 4){
+				shotsenemies.push(addshotsenemies(enemy));
+			}
+		}
+		if(enemy && enemy.state == 'hit'){
+			enemy.counter++;
+			if ( enemy.counter >= 20){
+				enemy.state = 'muerto';
+				enemy.counter = 0;
+			}
+		}
+	}
+	enemies = enemies.filter(function(enemy){
+		if(enemy && enemy.state != 'muerto' ) return true;
+		return false;
+	});
+}
+function moveshots(){
+	for (var i in shots) {
+		var Shooting = shots[i];
+		Shooting.y -= 2;
+	}
+	shots = shots.filter(function(Shooting){
+		return Shooting.y > 0;
+	});
+}
+function fire(){
+	shots.push({
+		x: ship.x + 20,
+		y: ship.y + 10,
+		width: 10,
+		height: 30
+	});
+}
