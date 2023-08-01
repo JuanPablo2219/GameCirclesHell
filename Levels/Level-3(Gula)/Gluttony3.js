b206d7b98a4016548fbd017c9275f07a6d5d47a3
Kevinquito7 <kmquito@sudamericano.edu.ec>
2023-08-01 10:49:15 -0500

    commit 5

--- Levels/Level-3(Gula)/Gluttony3.js
+++ Levels/Level-3(Gula)/Gluttony3.js
@@ -1,6 +1,5 @@
 const playBoard = document.querySelector(".play-board");
 const scoreElement = document.querySelector(".score");
-const highScoreElement = document.querySelector(".high-score");
 
 
 let gameOver = false;
@@ -10,8 +9,7 @@ let snakeBody =[];
 let velocityX =0, velocityY=0;
 let setIntervalId;
 let score = 0;
-let highScore = localStorage.getItem("high-score") || 0;
-highScoreElement.innerHTML = `High Score: ${highScore}`;
+
 
 
 const changeFoodPosition = () => {
@@ -45,6 +43,14 @@ if(e.key ==="ArrowUp"  && velocityY !=1){
 
 const initGame = () => {
     if(gameOver) return handleGameOver();
+
+if (snakeBody.length===21){
+    gameOver=true;
+    handleGameOver();
+    snakeBody();
+    return;
+}
+
     let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
     
     if(snakeX === foodX && snakeY === foodY){
@@ -52,10 +58,9 @@ const initGame = () => {
     snakeBody.push([foodX, foodY]);
     score++;
 
-    highScore = score >= highScore ? score : highScore;
-    localStorage.setItem("high-score", highScore);
+    
     scoreElement.innerHTML = `Score: ${score}`
-    highScoreElement.innerHTML = `High Score: ${highScore}`;
+    
     }
 
     for(let i = snakeBody.length -1; i > 0; i--){
@@ -72,6 +77,7 @@ const initGame = () => {
     if(snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30){
         gameOver = true;
     }
+
     
     for(let i=0; i < snakeBody.length; i++){
         htmlMarkup += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
@@ -84,5 +90,5 @@ const initGame = () => {
 }
 
 changeFoodPosition();
-setIntervalId = setInterval(initGame, 85);
+setIntervalId = setInterval(initGame, 80);
 document.addEventListener("keydown", changeDirection);
