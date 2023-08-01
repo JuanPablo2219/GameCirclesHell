d09a6931081a11f9024e894d4702850be58aedfd
unknown <jpmalla@sudamericano.edu.ec>
2023-08-01 08:54:51 -0500

    finish

--- Levels/Level-1(Limbo)/main.js
+++ Levels/Level-1(Limbo)/main.js
@@ -1,9 +1,9 @@
 //alerta inicio//
-
+let myaler = document.getElementById("myAlert");
 function closeAlert() {
     document.getElementById("myAlert").style.display = "none";
 }
-document.getElementById(myAlert).style.display = "block ";
+// document.getElementById(myAlert).style.display = "block ";
 
 function closeAlert() {
     let alert = document.getElementById("myAlert");
